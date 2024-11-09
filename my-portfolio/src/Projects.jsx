1eecd70ebc6bb0d04d2a74db287b3338a1cd1bd9
Daniel Yi <112517101+danielyime@users.noreply.github.com>
2024-11-08 22:25:26 -0500

    fixed spacing for scrolling

--- my-portfolio/src/Projects.jsx
+++ my-portfolio/src/Projects.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 
 const Projects = () => {
-
   return (
     <div className="min-h-screen bg-white">
       
@@ -27,7 +26,7 @@ const Projects = () => {
 
 
           {/* Intro Section */}
-          <section className="bg-gradient-to-br from-orange-400 to-pink-600 rounded-xl mb-8 p-8 text-white shadow-lg">
+          <section className="bg-gradient-to-br from-orange-400 to-pink-600 rounded-xl mb-24 p-8 text-white shadow-lg">
             <h2 className="text-2xl font-semibold mb-4">About my work</h2>
             <p className="text-white mb-4">
               I started my career at a small tech startup working in customer support. I became close with the product team, 
@@ -41,67 +40,43 @@ const Projects = () => {
 
           {/* Content Section */}
           <div className="space-y-24">
-            <section className="bg-white rounded-xl shadow-lg p-8 m-4" id="project1" >
-            <h2 className="text-2xl font-semibold text-gray-900 mb-4" >Project 1</h2>
-            <p className="text-gray-600 mb-4">
-              Work info
-            </p>
-          </section>
+            <section className="bg-white rounded-xl mb-8 p-8 text-black shadow-lg" id="project1">
+                <h2 className="text-2xl font-semibold mb-4">Project 1</h2>
+                <p className="text-gray-600 mb-4">
+                I started my career at a small tech startup working in customer support. I became close with the product team, 
+                highlighting pain-points and advocating for our users. Eventually, I got the opportunity to start sitting in on Product meetings and really 
+                </p>
+                <p className="text-gray-600">
+                I started my career at a small tech startup working in customer support. I became close with the product team, 
+                highlighting pain-points and advocating for our users. 
+                </p>
+            </section>
 
-          <section className="bg-white rounded-xl shadow-lg p-8 m-4" id="project2">
-            <h2 className="text-2xl font-semibold text-gray-900 mb-4">Project 2</h2>
-            <p className="text-gray-600 mb-4">
-              Work info
-            </p>
-            <p className="text-white">
-              I started my career at a small tech startup working in customer support. I became close with the product team, 
-              highlighting pain-points and advocating for our users. 
-            </p>
-            <p className="text-white">
-              I started my career at a small tech startup working in customer support. I became close with the product team, 
-              highlighting pain-points and advocating for our users. 
-            </p>
-            <p className="text-white">
-              I started my career at a small tech startup working in customer support. I became close with the product team, 
-              highlighting pain-points and advocating for our users. 
-            </p>
-            <p className="text-white">
-              I started my career at a small tech startup working in customer support. I became close with the product team, 
-              highlighting pain-points and advocating for our users. 
-            </p>
-            <p className="text-white">
-              I started my career at a small tech startup working in customer support. I became close with the product team, 
-              highlighting pain-points and advocating for our users. 
-            </p>
-          </section>
+            <section className="bg-white rounded-xl mb-8 p-8 text-black shadow-lg" id="project2">
+                <h2 className="text-2xl font-semibold mb-4">Project 2</h2>
+                <p className="text-gray-600 mb-4">
+                I started my career at a small tech startup working in customer support. I became close with the product team, 
+                highlighting pain-points and advocating for our users. Eventually, I got the opportunity to start sitting in on Product meetings and really 
+                </p>
+                <p className="text-gray-600">
+                I started my career at a small tech startup working in customer support. I became close with the product team, 
+                highlighting pain-points and advocating for our users. 
+                </p>
+            </section>
 
-          <section className="bg-white rounded-xl shadow-lg p-8 m-4" id="project3">
-            <h2 className="text-2xl font-semibold text-gray-900 mb-4">Project 3</h2>
-            <p className="text-gray-600 mb-4">
-              Work info
-            </p>
-            <p className="text-white">
-              I started my career at a small tech startup working in customer support. I became close with the product team, 
-              highlighting pain-points and advocating for our users. 
-            </p>
-            <p className="text-white">
-              I started my career at a small tech startup working in customer support. I became close with the product team, 
-              highlighting pain-points and advocating for our users. 
-            </p>
-            <p className="text-white">
-              I started my career at a small tech startup working in customer support. I became close with the product team, 
-              highlighting pain-points and advocating for our users. 
-            </p>
-            <p className="text-white">
-              I started my career at a small tech startup working in customer support. I became close with the product team, 
-              highlighting pain-points and advocating for our users. 
-            </p>
-            <p className="text-white">
-              I started my career at a small tech startup working in customer support. I became close with the product team, 
-              highlighting pain-points and advocating for our users. 
-            </p>
-          </section>
+            <section className="bg-white rounded-xl mb-8 p-8 text-black shadow-lg" id="project3">
+                <h2 className="text-2xl font-semibold mb-4">Project 3</h2>
+                <p className="text-gray-600 mb-4">
+                I started my career at a small tech startup working in customer support. I became close with the product team, 
+                highlighting pain-points and advocating for our users. Eventually, I got the opportunity to start sitting in on Product meetings and really 
+                </p>
+                <p className="text-gray-600">
+                I started my career at a small tech startup working in customer support. I became close with the product team, 
+                highlighting pain-points and advocating for our users. 
+                </p>
+            </section>
           </div>
+
         </div>
       </main>
     </div>
